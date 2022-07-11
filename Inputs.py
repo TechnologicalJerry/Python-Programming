59a311a0e6e5f337fd3e8d56b630d2c5a7cacc3c
technologicaljerry <cserajatverma@hotmail.com>
2022-07-11 21:24:56 +0530

    update

---
+++ Inputs.py
@@ -0,0 +1,17 @@
+x, y = [int(x) for x in input("Enter two values: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+print()
+
+x, y, z = [int(x) for x in input("Enter three values: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+print("Third Number is: ", z)
+print()
+
+x, y = [int(x) for x in input("Enter two values: ").split()]
+print("First number is {} and second number is {}".format(x, y))
+print()
+
+x = [int(x) for x in input("Enter multiple values: ").split()]
+print("Number of list is: ", x)
